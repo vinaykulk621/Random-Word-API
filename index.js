50035f000f8baa8db9fb4a2113327b16aa8395ea
vinay kulkarni <kulkarnivinay621@gmail.com>
2022-10-31 21:45:56 +0530

    make it better

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 module = require('./words');
 const PORT = process.env.PORT || 8000
 const express = require('express')
-
-// express
 const app = express()
 
 let words = module.words;
@@ -11,7 +9,6 @@ let words = module.words;
 app.get("/", (req, res) => {
     res.json({ response: `${words[Math.floor(Math.random() * words.length)]}` })
 })
-
 
 // the listening
 app.listen(PORT, () => console.log(`https://localhost:${PORT}`))
